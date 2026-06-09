b1dc0f46e36329d210d1903a7110119708299fa6
Nguyen Van Thang <emailcuaban@gmail.com>
2026-06-09 16:32:46 +0700

    fix ring

--- src/constants/index.js
+++ src/constants/index.js
@@ -80,7 +80,7 @@ export const calculateSizes = (isSmall, isMobile, isTablet) => {
     
     cubePosition: isSmall ? [4, -5, 0] : isMobile ? [5, -5, 0] : isTablet ? [5, -5, 0] : [9, -5.5, 0],
     reactLogoPosition: isSmall ? [3, 4, 0] : isMobile ? [5, 4, 0] : isTablet ? [5, 4, 0] : [14, 6, -2],
-    ringPosition: isSmall ? [-5, 7, 0] : isMobile ? [-10, 10, 0] : isTablet ? [-35, 10, -10] : [-40, 10, -15],
+ringPosition: isSmall ? [-5, 7, 0] : isMobile ? [-10, 10, 0] : isTablet ? [-12, 10, 0] : [-15, 10, 0],
     targetPosition: isSmall ? [-5, -10, -10] : isMobile ? [-9, -10, -10] : isTablet ? [-11, -7, -10] : [-10, -10, -10],
   };
 };
